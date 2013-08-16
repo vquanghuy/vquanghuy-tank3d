6eea7bcce1f00900302d5fbf5393c951bb44d815
vquanghuy@gmail.com <vquanghuy@gmail.com@10cff659-d1e4-0ea4-f975-ba678a7c9e32>
2013-08-16 03:43:15 +0000

    + [3DMD] replace makedata with generate_ui

--- trunk/tools/3DMD/data/makedata.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM =================================
-REM ======= GENERATE UI FILE ========
-REM =================================
-%QTDIR%\5.1.0\msvc2012\bin\uic.exe -o "..\src\generated\ui_mainwindow.h" ".\forms\mainwindow.ui"
